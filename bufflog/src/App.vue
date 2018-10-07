360d52a86b060b670abd2bfbbcd4aa7bc1420912
sarafarmer1 <sbfarmer@my.milligan.edu>
2018-10-07 16:16:39 -0400

    Added header

--- bufflog/src/App.vue
+++ bufflog/src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <Header/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Header from './components/Header.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    Header
   }
 }
 </script>
@@ -21,7 +20,7 @@ export default {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
+  text-align:left;
   color: #2c3e50;
   margin-top: 60px;
 }
---
+++ bufflog/src/components/Header.vue
@@ -0,0 +1,44 @@
+<template>
+    <div id="header">
+        <span v-html="createPost" id="create-post-link"></span>
+        <span v-html="contactRequests" id="contact-requests-link"></span>
+        <input type="button" v-model="msg" @click="clicking()" />
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                msg: "Log in",
+                createPost: '',
+                contactRequests: ''
+            };
+        },
+        methods: {
+            clicking() {
+                if(this.msg == "Log In") {
+                    this.msg = "Sign Out";
+                    this.createPost = '<a href="#">Create Post</a>';
+                    this.contactRequests = '<a href="#"> Contact Requests</a>';
+                } else {
+                    this.msg = "Log In";
+                    this.createPost = '';
+                    this.contactRequests = '';
+                }
+            }
+        }
+    }
+    </script>
+
+    <style scoped>
+    #header {
+        position:absolute;
+        width: 100%;
+        height: 50px;
+        background-color:maroon;
+        top: 0;
+        left: 0;
+        padding: 20px;
+    }
+    </style>
